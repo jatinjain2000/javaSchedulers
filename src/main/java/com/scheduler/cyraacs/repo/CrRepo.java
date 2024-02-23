216d7c80c46b8ba9b2b56bde86c310f551f7af4d
jatinjain2000 <jatinjain.2011@gmail.com>
2024-02-23 14:58:01 +0530

    first commit

---
+++ src/main/java/com/scheduler/cyraacs/repo/CrRepo.java
@@ -0,0 +1,14 @@
+package com.scheduler.cyraacs.repo;
+
+import com.scheduler.cyraacs.models.ControlRequirements;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+
+
+@Repository
+public interface CrRepo extends JpaRepository<ControlRequirements,Integer> {
+
+            ControlRequirements findByControlId(Integer id);
+}
